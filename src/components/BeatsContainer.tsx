83c36e08edbb3742ec81f030ecb628d5cd4c790b
RudeBon <yumuema@gmail.com>
2021-01-27 21:15:55 +0200

    added start/stop btn that controls ticking

--- src/components/BeatsContainer.tsx
+++ src/components/BeatsContainer.tsx
@@ -1,26 +1,87 @@
-import React, {useState, useEffect} from 'react'
-import SingleBeatBlock from './SingleBeatBlock'
+import React, { useState, useEffect, useRef } from 'react'
+// import SingleBeatBlock from './SingleBeatBlock'
 
-const BeatsContainer: React.FC = () => {  
-    const [beats, setBeats] = useState<number[]>([0,1,2,3]);
-    
+const BeatsContainer: React.FC = () => {
+    const [beats, setBeats] = useState<number[]>([0, 1, 2, 3, 4]);
+
+    const [timeout, setTimeout] = useState(0);
+    const [activeId, setActiveId] = useState<number>(0);
+    const [isActive, setIsActive] = useState<boolean>(false);
+
+    const [intervalValue, setintervalValue] = useState<number>(2000);
+
+    useEffect(() => {
+        console.log('after mounting isTicking', isActive);
+
+        // let interval = null
+        if (isActive) {
+            setTimeout(window.setInterval(() => {
+                setActiveId(activeId => activeId + 1);
+                console.log('tick', activeId);
+            }, intervalValue))
+        } else if (!isActive /*&& seconds !== 0*/) {
+            clearInterval(timeout);
+        }
+        return () => clearInterval(timeout);
+    }, [isActive, activeId])
+
 
     const beatBlocks = () => {
         return beats.map((num) => {
-            console.log(num);            
-            return <li key={num}>
-                <SingleBeatBlock/>
+            const classes = ['beat']
+            if (num === activeId) {
+                classes.push('activeBeat')
+            }
+            console.log(num);
+            return <li key={num} >
+                <div className={classes.join(' ')}></div>
+                {/* <SingleBeatBlock/> */}
             </li>
         })
     }
 
+    const startTicking = (event: React.MouseEvent) => {
+        setIsActive(true)
+        console.log('after click on btn', isActive);
+        // const ticker = setInterval(() => {
+        //     console.log('tick');
+        // }, intervalValue)
+    }
+
+    const stopTicking = (event: React.MouseEvent) => {
+        setIsActive(false)
+        // setActiveId(0)      
+        console.log('after click on btn', isActive);
+        // clearInterval(ticker)       
+    }
+
+    // const onButtonClick = () => {
+    //     setIsTicking(prev => !prev)
+    //     console.log('after click on btn', isTicking);
+
+    //     const ticker = setInterval(() => {
+    //                 console.log('tick');
+    //             }, intervalValue)
+    //     if (!isTicking) {
+    //         clearInterval(ticker)
+    //         console.log('interval stop');            
+    //     }
+    // }
+
     return (
         <div className="beatsBlock">
+            <h3>Actual beat: {activeId}</h3>
             <ul className='beats_ul'>
-            {beatBlocks()}
+                {beatBlocks()}
             </ul>
+
+            {!isActive
+                ? <button onClick={startTicking}>Start</button>
+                : <button onClick={stopTicking}>Stop</button>}
+
+            {/* <button onClick={onButtonClick}>{!isTicking ? 'Start' : 'Stop'}</button> */}
         </div>
     )
 }
 
-export default BeatsContainer;
+export default BeatsContainer; 
