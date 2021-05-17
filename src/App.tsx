e677e444d7450cc059e7feb36ec2d2f30da1f281
RudeBon <yumuema@gmail.com>
2021-05-17 10:45:50 +0300

    no need in mobilestub anymore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css';
 import ContainerComponent from './components/ContainerComponent';
-import MobileStub from './components/MobileStub';
 
 export default class App extends React.Component<{}> {
   public render() {
